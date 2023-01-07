35e512ce33d6aaf227273d019ba9824c7887eb73
Gabriel Barreto <88603031+Cyacer@users.noreply.github.com>
2023-01-07 05:26:08 -0300

    Atualizacao

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-no-bind */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-props-no-multi-spaces */
 import React from 'react';
 import db from '../db.json';
 import QuizContainer from '../src/components/QuizContainer';
